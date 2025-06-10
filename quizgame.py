a7ba3f4410d9a3fcb6dc3d5631b84aca03cb103f
PurpleLightSpeed <lotaagulue@gmail.com>
2025-06-10 15:45:53 -0400

    Introducing a simple quiz game in Python!

---
+++ quizgame.py
@@ -0,0 +1,10 @@
+print("Welcome to the Quiz Game!")
+
+playing = input("Do you want to play? (yes/no): ")
+
+if playing != "yes":
+    # quit() function exits the program
+    quit()
+
+print("Great! Let's start the game.")
+
